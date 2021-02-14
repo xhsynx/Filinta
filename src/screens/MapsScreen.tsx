1a11491df413ba531a900b9e8d3c006cedf4a2d2
Hüseyin Akçiçek <48623859+xhsynx@users.noreply.github.com>
2021-02-15 02:57:36 +0300

    Google Maps

--- src/screens/MapsScreen.tsx
+++ src/screens/MapsScreen.tsx
@@ -1,30 +1,24 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, Dimensions } from "react-native";
+import { Text, View } from "../components/Themed";
 
 export default function MapsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <MapView style={styles.map} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
